437649cce9b60cab9cba570cc17a7f8c55460193
Barragan180497 <fabaes18@hotmail.com>
2021-03-30 10:26:36 -0500

    Clase_26_03_2021

---
+++ main.js
@@ -0,0 +1,25 @@
+// Variables y constantes
+
+let nombre;
+let apellido;
+const edad = 23;
+nombre = "Fabian";
+apellido = "Barragan";
+console.log(nombre, apellido, edad);
+
+
+// Tipos de datos
+let Nombre = `Fabian Andres Barragan Espinosa`; //String
+let Edad = 23; //number - intenger - int;
+let altura = 1.70; //Double - float
+let profesor = true; // Boleano
+let informacion = [Nombre, Edad, altura, profesor]; //Array
+console.log(informacion);
+// console.log(informacion[2]);
+let baso = {
+	material : "porcelan",
+	diametro :  60,
+	lleno : false
+}; // Object
+console.log(baso);
+// console.log(baso.diametro);
